76585a5dae5a41696f3ba3f820c27e3105f60171
Frederick Articuno <mckiyoshi@gmail.com>
2019-09-20 11:26:00 -0400

    ported HW to  Trains

--- TrainSchedule/Assets/app.js
+++ TrainSchedule/Assets/app.js
@@ -20,7 +20,7 @@
                 input text
                 SUBMIT BUTTON   
 */
-
+/*
 var trains = [];
 
 trains.push('Lambtrak',
@@ -37,7 +37,7 @@ inputfields.push (
     'firsttraintime',
     'frequency',
 );
-alert('JS read');
+document.write('JS read success');
 function MainDisplay() {
     // build jumbotron
     var jumbotron = $('<div>');
@@ -97,3 +97,63 @@ function MainDisplay() {
     }
 }
 MainDisplay();
+*/
+
+///
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAQSWHrrXes_C3tMYqhF_dia7C3jI2wMkc",
+    authDomain: "starterfka.firebaseapp.com",
+    databaseURL: "https://starterfka.firebaseio.com",
+    projectId: "starterfka",
+    storageBucket: "",
+    messagingSenderId: "584403285589",
+    appId: "1:584403285589:web:12f58acc15bb74cd6f46dc"
+};
+// var then = moment();
+// var howlong = moment().then.fromNow();
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+var name = 0;
+var rate = 0;
+var date = 0;
+var monthly = 0;
+
+//database.ref().on('value',function(snapshot){
+    //  On DB change
+//});
+
+//$('.submit-button').on('click',function() {
+$('.submit-button').on('click',function() {
+// function clickSubmit() { 
+    console.log("clicked");
+    event.preventDefault();
+    name = $('#user-entry-name').val();
+    rate = $('#user-entry-rate').val();
+    date = $('#user-entry-start').val();
+    monthly = $('#user-entry-monthly').val();
+
+    alert(`Train: ${name} submitted.`);
+// Push new values to 'table'
+    function mkp(value){
+        return '<p>'+ value + '</p>'
+    }
+    $('#em-name').append(mkp(name));
+    $('#em-rate').append(mkp(rate));
+    $('#em-start').append(mkp(date));
+    $('#em-worked').append(mkp(5));
+    $('#em-monthly').append(mkp(monthly));
+    $('#em-total').append(mkp(monthly*5));
+
+    database.ref().push({
+        user:{
+            name:name,
+            rate:rate,
+            date:date,
+            monthly:monthly,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP,
+        }    
+    });
+});
